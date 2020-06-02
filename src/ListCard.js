006bb1a04cf0f46adb4e15a5d455a54e1020523e
KyungjinJPark <kjparkcontact@gmail.com>
2020-06-01 23:37:21 -0400

    can delete numbers by X button

--- src/ListCard.js
+++ src/ListCard.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
-import { Container, AppBar, Toolbar, List, ListItem, Checkbox } from "@material-ui/core";
+import { Toolbar, List, ListItem, Checkbox, IconButton } from "@material-ui/core";
+import ClearIcon from '@material-ui/icons/Clear';
+
 import { unfold } from "ramda";
 
 import "./ListCard.css";
 import TopBar from "./TopBar";
 
 const ListCard = () => {
-  const [items] = useState(() =>
+  const [items, setItems] = useState(() =>
     unfold((n) => (n > 100 ? false : [n, n + 1]), 0)
   );
 
@@ -16,10 +18,15 @@ const ListCard = () => {
       <TopBar />
       <Toolbar />
       <List>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <ListItem key={item} divider>
             <Checkbox color="primary"/>
-            {item}
+            <span style={{flex: 1}}>{item}</span>
+            <IconButton aria-label="delete">
+                <ClearIcon onClick={() => setItems((prevItems) => {
+                  return prevItems.filter((num) => item !== num);
+                })} />
+            </IconButton>
           </ListItem>
         ))}
       </List>
