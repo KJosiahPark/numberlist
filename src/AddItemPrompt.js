5b1f15b279d32d556e2d16371c9236cb5c4c1940
KyungjinJPark <kjparkcontact@gmail.com>
2020-06-04 22:36:13 -0400

    clear selected and delete selected in toolbar work

---
+++ src/AddItemPrompt.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddItemPrompt = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default AddItemPrompt;
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,21 +1,41 @@
 import React from "react";
 
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
 import SortIcon from "@material-ui/icons/Sort";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
 
-const TopBar = () => {
+const TopBar = ({selected, setSelected, deleteItem}) => {
+
+  const clearSelected = () => {
+    setSelected([]);
+  }
+
+  const deleteSelected = () => {
+    selected.forEach(item => {
+      deleteItem(item);
+    });
+    clearSelected();
+  }
+
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6" style={{flex: 1}}>
           Nombres
         </Typography>
+        <IconButton
+          color="inherit"
+          onClick={clearSelected} >
+          <IndeterminateCheckBoxIcon />
+        </IconButton>
         <IconButton color="inherit">
           <SortIcon />
         </IconButton>
-        <IconButton color="inherit">
+        <IconButton
+          color="inherit"
+          onClick={deleteSelected} >
           <DeleteIcon />
         </IconButton>
         <IconButton color="inherit">
